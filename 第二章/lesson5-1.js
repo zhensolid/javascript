b67e5032ba337cc96e6aca72b872de615a7f2c67
zhensolid <zhensolid@vip.qq.com>
2021-09-19 23:53:43 +0800

    update

---
+++ 第二章/lesson5-1.js
@@ -0,0 +1,16 @@
+var alias = "小飞侠";
+var height = 198;
+var score = 33643;
+var achievement = "五届NBA总冠军";
+var position = "得分后卫";
+
+document.write("<br>别名:");
+document.write(alias);
+document.write("<br>身高:");
+document.write(height);
+document.write("厘米<br>总得分:");
+document.write(score);
+document.write("分<br>成就:");
+document.write(achievement);
+document.write("<br>场上位置:");
+document.write(position);
